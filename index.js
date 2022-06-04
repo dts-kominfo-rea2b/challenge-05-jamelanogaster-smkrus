4449f37ae97102103290efc3c704631682956fa7
jamelanogaster-smkrus <jamil@smkrus.sch.id>
2022-06-04 08:14:03 +0700

    menambahkan jawaban

--- index.js
+++ index.js
@@ -6,15 +6,21 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (data, operator) => {
+const result  = operator(data);
+  return result.map((item, index) => `${index+1}. ${item}`);
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
-
+const sortAscending = (data) => {
+  return data.sort()
+}
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (data) => {
+  return data.sort().reverse()
+}
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
